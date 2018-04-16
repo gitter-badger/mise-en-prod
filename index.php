ad823feded04d07edf4711c1ef975e1c883da8ac
Mehdy Driouech <mehdy.driouech@oxo-team.com>
2018-04-16 09:44:58 +0200

    Some cleaning
    functions.php get the tracking functions
    index.php keep the minimal functions to work

--- index.php
+++ index.php
@@ -31,6 +31,8 @@
 
 <?php
 
+    include 'functions.php';
+
 	echo"<p class='titre'>Est-ce qu'on met en prod aujourd'hui ?</p>";
 
 if (date("l")=="Monday"){	
@@ -157,72 +159,9 @@ if (date("l")=="Monday"){
 	echo'ERREUR';
 }
 
-	//compteur page vue
+    compteurvisite();
+    IpVisiteur();
 
-	if(file_exists('compteur.txt'))
-	{
-        $compteur_f = fopen('compteur.txt', 'r+');
-        $compte = fgets($compteur_f);
-	}
-	else
-	{
-        $compteur_f = fopen('compteur.txt', 'a+');
-        $compte = 0;
-	}
-	$compte++;
-	fseek($compteur_f, 0);
-	fputs($compteur_f, $compte);
-	fclose($compteur_f);
-	
-	//adresse IP du visiteur - Fournisseur d'acces internet
-	
-	$file="ip.txt";
-    $currentFile=fopen($file,'a');
-    $current="";
-    $today = new DateTime('NOW', new DateTimeZone('Europe/Paris'));
-    $today = $today->format('d/m/Y - H:i:s');
-    $ipAdress = $_SERVER['REMOTE_ADDR'];
-
-    // Fai part
-
-    try {
-
-        $url = "http://ip-api.com/json/".$ipAdress;
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json')); // Assuming you're requesting JSON
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $response = curl_exec($ch);
-        $resultData = json_decode($response, true);
-
-        if ($status = $resultData[status]){
-            $fai = $resultData[isp];
-            $city = $resultData[city];
-            $region = $resultData[regionName];
-            $codePostal = $resultData[zip];
-        } else {
-            $fai = "failed to get";
-            $city = "failed to get";
-            $region = "failed to get";
-            $codePostal = "failed to get";
-        }
-
-
-    } catch (Exception $e) {
-        echo 'Exception reçue : ',  $e->getMessage(), "\n";
-    }
-
-    // End Fai part
-            
-    try {                  
-			$current .= $today ." - ". $ipAdress." Ville : ".$city." Code postal : ".$codePostal." Region : ".$region." FAI : ".$fai."\n";
-        } catch (Exception $e) {
-			$current .= $today.'Exception reçue'.'\n' ;
-        }
-	fwrite($currentFile,$current);
-	fclose($currentFile);
-	echo ""; //close call
 	
 	// affichage page
 	
