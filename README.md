9494e77067e581526ae88f8f1efd9e4150367c85
Mehdy Driouech <mehdy.driouech@oxo-team.com>
2018-04-16 09:21:59 +0200

    Tracking more precise (still dirty)

--- README.md
+++ README.md
@@ -6,5 +6,6 @@
 - Langage : php
 - Version : Dirty v1
 - Functionnality : Differents gifs for each day will tell you if it's a good day to deploy your code into a production server
+- Tracking : IP of each visitor will be saved into log file (Just for curiosity] also interact with an geo ip api to get more infos (like ISP, City etc.)
 
 licence : Feel free to do what you want. Fork the repo and have fun, Just give me the link of your git repos at my mail adress, i'll be curious to see how you improve the code :)
--- index.php
+++ index.php
@@ -174,19 +174,51 @@ if (date("l")=="Monday"){
 	fputs($compteur_f, $compte);
 	fclose($compteur_f);
 	
-	//adresse IP du visiteur
+	//adresse IP du visiteur - Fournisseur d'acces internet
 	
 	$file="ip.txt";
     $currentFile=fopen($file,'a');
     $current="";
-    $today = new DateTime('NOW');
+    $today = new DateTime('NOW', new DateTimeZone('Europe/Paris'));
     $today = $today->format('d/m/Y - H:i:s');
-    $ipAdress = $_SERVER['REMOTE_ADDR'];		
+    $ipAdress = $_SERVER['REMOTE_ADDR'];
+
+    // Fai part
+
+    try {
+
+        $url = "http://ip-api.com/json/".$ipAdress;
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json')); // Assuming you're requesting JSON
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $response = curl_exec($ch);
+        $resultData = json_decode($response, true);
+
+        if ($status = $resultData[status]){
+            $fai = $resultData[isp];
+            $city = $resultData[city];
+            $region = $resultData[regionName];
+            $codePostal = $resultData[zip];
+        } else {
+            $fai = "failed to get";
+            $city = "failed to get";
+            $region = "failed to get";
+            $codePostal = "failed to get";
+        }
+
+
+    } catch (Exception $e) {
+        echo 'Exception reçue : ',  $e->getMessage(), "\n";
+    }
+
+    // End Fai part
             
     try {                  
-			$current .= $today ." - ". $ipAdress."\n";		                
+			$current .= $today ." - ". $ipAdress." Ville : ".$city." Code postal : ".$codePostal." Region : ".$region." FAI : ".$fai."\n";
         } catch (Exception $e) {
-			$current .= $today.' URL HS - Exception reçue'.'\n' ; 
+			$current .= $today.'Exception reçue'.'\n' ;
         }
 	fwrite($currentFile,$current);
 	fclose($currentFile);
